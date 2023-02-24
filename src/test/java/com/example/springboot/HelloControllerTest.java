d83b73ec79168036aa03c5dc701c87832859ee66
Viktor van den Berg <viktorvdberg@yahoo.com>
2023-02-24 21:33:34 +0100

    update

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -27,6 +27,6 @@ class HelloControllerTest {
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from Spring Boot + Tanzu"));
+            .andExpect(content().string("Greetings to Stockholm from Spring en TAP!"));
     }
 }
