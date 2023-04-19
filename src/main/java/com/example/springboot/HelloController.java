0ecd49325c30065a8fa0c118cfb2f393dfefeef2
Viktor van den Berg <viktorvdberg@yahoo.com>
2023-04-19 19:09:34 +0200

    update at kubecon

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Tanzu Application Catalog!";
+		return "Greetings from Tanzu Application Platform to Kubecon!";
 	}
 
 }
--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -27,6 +27,6 @@ class HelloControllerTest {
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from Tanzu Application Catalog!"));
+            .andExpect(content().string("Greetings from Tanzu Application Platform to Kubecon!"));
     }
 }
