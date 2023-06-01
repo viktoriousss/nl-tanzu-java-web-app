e556b4b9ae265a02b083677350f72b6ad42ff80a
Viktor van den Berg <viktorvdberg@yahoo.com>
2023-06-01 14:50:51 +0200

    update

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Tanzu Application Platform!";
+		return "Greetings from Tanzu Application Platform to The Netherlands!";
 	}
 
 }
