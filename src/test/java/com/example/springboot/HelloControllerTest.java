d1579963159312ed22e8d8ef6d11917630b16c8b
Viktor van den Berg <viktorvdberg@yahoo.com>
2023-07-06 10:49:48 +0200

    update in Delft

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -27,6 +27,6 @@ class HelloControllerTest {
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from Tanzu Application Platform to Ernst!"));
+            .andExpect(content().string("Greetings from Tanzu Application Platform to Delft!"));
     }
 }
