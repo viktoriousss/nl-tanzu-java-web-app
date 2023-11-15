ad44541a2abbe47957ee9158c65ec0c41f88a473
coyte <niels@teekens.info>
2023-11-15 20:20:12 +0100

    edit string

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -11,7 +11,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Tanzu Application Platform to Sweden.";
+		return "Greetings from Tanzu Application Platform to SwedenNIELS";
 	}
 
 }
