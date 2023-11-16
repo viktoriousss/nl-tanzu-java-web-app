489281df0987a1eedb76658e3e722b59facf8e97
coyte <niels@teekens.info>
2023-11-16 11:14:47 +0100

    edited string

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -28,6 +28,6 @@ class HelloControllerTest {
         mockMvc
                 .perform(get("/"))
                 .andExpect(status().isOk())
-                .andExpect(content().string("Greetings from Tanzu Application Platform to Belgium!"));
+                .andExpect(content().string("Greetings from Tanzu Application Platform to Sweden!!"));
     }
 }
