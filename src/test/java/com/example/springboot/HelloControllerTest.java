d9e934a6c62292aa77d6647f5ff2d99af008ff94
Viktor van den Berg <viktorvdberg@yahoo.com>
2023-12-13 12:28:51 +0100

    update

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -21,13 +21,9 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        // It look this line is not necessary and does the same as the mockMvc part
-        // assertEquals("Greetings to Sweco from Spring Boot + Tanzu!",
-        // controller.index());
-
         mockMvc
                 .perform(get("/"))
                 .andExpect(status().isOk())
-                .andExpect(content().string("Greetings from Tanzu Application Platform!!"));
+                .andExpect(content().string("Greetings from Tanzu Application Platform!"));
     }
 }
